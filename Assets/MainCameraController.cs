787a421ee1cf87099fb33eac63738c87000110e9
apeluso03 <124719953+apeluso03@users.noreply.github.com>
2025-02-09 16:10:24 -0500

    Finished setup and added joke

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why did the game developer go broke? Because they used up all their cache trying to render their assets!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
